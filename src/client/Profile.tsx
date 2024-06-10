0634bedc56c55721c9e3af2c312ab8ba87bdd3fe
dvh5561 <22529166+Ulidtsoa01@users.noreply.github.com>
2024-06-10 05:44:18 -0400

    start profile-gallery

--- src/client/Profile.tsx
+++ src/client/Profile.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import './Profile.css';
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -8,12 +9,33 @@ const Profile = () => {
     return <div>Loading ...</div>;
   }
 
+  //isAuthenticated && user &&
   return (
-    isAuthenticated && user && (
+    isAuthenticated &&
+    user && (
       <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+        <div className="box">
+          <img src={user.picture} alt={user.name} />
+          <div className="box-info">
+            <div className="head colhead">{user.name}</div>
+            <div className="box-row">
+              <ul className="summary list-name nobullet">
+                <li>Name</li>
+                <li>Email</li>
+                <li>Location</li>
+                <li>Organization</li>
+                <li>Website</li>
+              </ul>
+              <ul className="summary list-value nobullet">
+                <li>{user.name}</li>
+                <li>{user.email}</li>
+                <li>Text</li>
+                <li>Text</li>
+                <li>Text</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     )
   );
