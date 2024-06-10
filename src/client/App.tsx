f11f7937c778bb8d0ecfd1d8f87eff87a22c7adf
AmritaMohandas <amrita.a.mohandas@gmail.com>
2024-06-10 17:10:48 -0500

    initial commit

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage';
 import NotFoundPage from './NotFoundPage';
+import Profile from './Profile';
 
 const App: React.FC = () => {
     return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="*" element={<NotFoundPage />} />
+                    <Route path="/profile" element={<Profile />} />
                 </Routes>
             </div>
         </Router>
