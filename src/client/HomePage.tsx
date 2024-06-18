3d5c2fd38094232408968b0281c52259459fca1f
Ryan Mohammadian <ryanm@Ryans-MacBook-Pro-9.local>
2024-06-18 14:54:18 -0500

    React Routing for profile edit page

--- src/client/HomePage.tsx
+++ src/client/HomePage.tsx
@@ -1,16 +1,23 @@
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 import Profile from "./Profile";
 
 const HomePage = () => {
+  const { isAuthenticated } = useAuth0();
   return (
     <div>
       <h1>Home Page</h1>
       <p>This is the home page!</p>
-      <Profile />
-      <LoginButton />
-      <LogoutButton />
+
+      {isAuthenticated ? (
+        <li><Link to="/profile-edit">Edit Your Profile</Link></li>
+      ) : (null)}
+
+      {isAuthenticated ? (
+        <LogoutButton />
+      ) : (<LoginButton />)}
     </div>
   );
 }
