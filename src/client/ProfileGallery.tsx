40f02df92660b65e52ccad27dd245ca7d8efa9d7
“Ulidtsoa01” <22529166+Ulidtsoa01@users.noreply.github.com>
2024-06-19 05:02:57 -0400

    incomplete auth0 requests

--- src/client/ProfileGallery.tsx
+++ src/client/ProfileGallery.tsx
@@ -1,8 +1,73 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useState, useEffect } from 'react';
 import './ProfileGallery.css';
 import Header from './Header';
 import Footer from './Footer';
+// import auth from './constants';
+const auth = {
+  domain: 'dev-cabv17sajsvsytoj.us.auth0.com',
+  client_id: '3QtlFcwJrplRcVzy3Y6dTVrtkr39MO7K',
+};
+
+const getMgmtAPIAccessToken = () => {
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const options = {
+    method: 'POST',
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    data: new URLSearchParams({
+      grant_type: 'client_credentials',
+      client_id: auth.client_id,
+      client_secret: '{yourClientSecret}',
+      audience: `https://${auth.domain}/api/v2/`,
+    }),
+  };
+
+  useEffect(() => {
+    fetch(`https://${auth.domain}/oauth/token`, options)
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error('server error');
+        }
+        return response.json();
+      })
+      .then((response) => setResult(response.access_token))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { result, error, loading };
+};
+
+const userSearch = () => {
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const options = {
+    method: 'GET',
+    params: { sort: 'created_at:1', search_engine: 'v3' },
+    headers: { authorization: 'Bearer {yourMgmtApiAccessToken}' },
+  };
+
+  useEffect(() => {
+    fetch(`https://${auth.domain}/api/v2/users`, options)
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error('server error');
+        }
+        return response.json();
+      })
+      .then((response) => setResult(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { result, error, loading };
+};
 
 const ProfileResult = () => {
   const { user } = useAuth0();
