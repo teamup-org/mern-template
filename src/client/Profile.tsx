f0c6a18eafdc35d9bd73dd44362b15d70f74a569
AmritaMohandas <amrita.a.mohandas@gmail.com>
2024-06-26 14:27:49 -0500

    added basic styling to profile view

--- src/client/Profile.tsx
+++ src/client/Profile.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Header from './Header';
+import './Profile.css';
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -14,9 +15,13 @@ const Profile = () => {
     user && (
       <div>
         <Header />
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+        <div className="content">
+          <div className="profile-container">
+            <img src={user.picture} alt={user.name} />
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
+        </div>
       </div>
     )
   );
