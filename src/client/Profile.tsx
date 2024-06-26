0241cf08373551a54d79891642eeeb91e94b898f
AmritaMohandas <amrita.a.mohandas@gmail.com>
2024-06-26 14:35:44 -0500

    added historyapifallback to reload of profile view

--- src/client/Profile.tsx
+++ src/client/Profile.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, Navigate } from "react-router-dom";
 import { useAuth0 } from '@auth0/auth0-react';
 import Header from './Header';
 import './Profile.css';
@@ -7,7 +8,7 @@ const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
-    return <div>Loading ...</div>;
+    return <Navigate to="/" />;
   }
 
   return (
