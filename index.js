bb4fb7984fa19aad12a86b1036f4cc1ad048cdf2
andrewoh365 <andrewoh365@gmail.com>
2024-07-16 23:38:34 -0500

    Created a page that can extract text from a pdf

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ const app = express();
 app.use("/", express.static("public"));
 app.use(fileUpload());
 
+app.post("/extract-text", (req, res) => {
+  if (!req.files && !req.files.pdfFile) {
+    res.status(400);
+    res.end();
+  }
+
+  pdfParse(req.files.pdfFile).then(result => {
+    res.send(result.text);
+  });
+});
+
 app.listen(3000);
