eb7cdbec308a45182676206301534a351ed302c5
Arivera919 <Arivera919@outlook.com>
2024-07-23 14:19:05 -0500

    onto something

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -6,17 +6,20 @@ import ProfileGallery from './ProfileGallery';
 import Profile from './Profile';
 import NotFoundPage from './NotFoundPage';
 import AIProfile from './AIProfile';
+import TeacherHomePage from './TeacherHomePage';
+import StudentViewPage from './StudentViewPage';
 
 const App: React.FC = () => {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<TeacherHomePage />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/profile-gallery" element={<ProfileGallery />} />
           <Route path="/ai-profile" element={<AIProfile />} />
           <Route path="*" element={<NotFoundPage />} />
+          <Route path='/student' element={<StudentViewPage />} />
         </Routes>
       </div>
     </Router>
