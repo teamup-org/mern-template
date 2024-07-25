b82886122b559dfdc6bdc1fd596ca9f1272fbe34
Ruiming Chen <b150540215@tamu.edu>
2024-07-25 18:48:10 -0500

    merged

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,16 @@
-import express from 'express';
-import path from 'path';
-
-import connectDB from './helpers/db';
-import userRoutes from './routes/user';
-import docRoutes from './routes/doc';
+import cors from "cors";
+import express from "express";
+import path from "path";
+import connectDB from "./helpers/db";
+import docRoutes from "./routes/doc";
+import teacherRoutes from "./routes/teacher";
+import userRoutes from "./routes/user";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 async function main() {
-  await connectDB();
+	await connectDB();
 }
 
 main().catch(console.error);
@@ -17,24 +18,32 @@ main().catch(console.error);
 app.use(express.json());
 
 // ROUTES
-app.use('/api/users', userRoutes);
-app.use('/api/docs', docRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/docs", docRoutes);
+app.use("/api/teacher", teacherRoutes);
 
+app.use(
+	cors({
+		origin: "http://localhost:8080", // replace with your frontend URL
+		methods: ["GET", "POST", "PUT", "DELETE"], // add the HTTP methods you're using
+		allowedHeaders: ["Content-Type", "Authorization"], // add the headers you're using
+	})
+);
 // Serve static files from the 'dist' directory
-app.use(express.static(path.join(__dirname, '..')));
+app.use(express.static(path.join(__dirname, "..")));
 
 // Example of a simple API endpoint (order matters)
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello, world!' });
+app.get("/api", (req, res) => {
+	res.json({ message: "Hello, world!" });
 });
 
 // Fallback for SPA routing
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'index.html'));
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "..", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 export default app;
